5592021f1c7f073cc510a0b7882156e9c0058453
yzne <yznsalameh@gmail.com>
2025-02-19 09:17:41 +0400

    GettingStarted

---
+++ CreatePolylineFromMultiplePoints/GettingStarted/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+
+namespace GettingStarted
+{
+    public class Class1 : IExtensionApplication
+    {
+        #region IExtensionApplication Members
+
+        public void Initialize()
+        {
+            throw new Exception("The method is not implemented.");
+        }
+
+        public void Terminate()
+        {
+            throw new Exception("The method or operation is not implemented.")
+        }
+        #endregion
+    }
+}
